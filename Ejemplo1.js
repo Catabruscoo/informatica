90f8a82f3bcd7a51e62781824a4a623f099c0292
Catabruscoo <c.brusco@colegiocalasanz.edu.ar>
2025-09-25 16:37:44 -0300

    Add files via upload

---
+++ Ejemplo1.js
@@ -0,0 +1,10 @@
+function pedirNombre() {
+    let nombre = prompt("¿Cómo te llamas?");
+    alert("Hola " + nombre + ", ¡bienvenido a la clase de JavaScript!");
+   }
+   
+   if (nombre) {
+    alert("Hola " + nombre + ", ¡bienvenido a la clase de JavaScript!");
+    } else {
+    alert("No escribiste tu nombre, inténtalo de nuevo.");
+    }
